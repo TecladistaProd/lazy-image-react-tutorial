9a33bb280bdb3cf802b36602944a0b53517e85d3
Vitor Cruz <vitor.dasilvacruz@gmail.com>
2020-02-10 18:54:07 -0300

    Updates

---
+++ precache-manifest.4f0e2a5cbaa00f2af848c5a1467808ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e118969c53de1f7bce256df4d8b1818",
+    "url": "/lazy-image-react-tutorial/index.html"
+  },
+  {
+    "revision": "6db92c04681047342a83",
+    "url": "/lazy-image-react-tutorial/static/css/main.1e30ee1b.chunk.css"
+  },
+  {
+    "revision": "fd066682cca0371c85b6",
+    "url": "/lazy-image-react-tutorial/static/js/2.5da056f2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lazy-image-react-tutorial/static/js/2.5da056f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db92c04681047342a83",
+    "url": "/lazy-image-react-tutorial/static/js/main.9898ce7d.chunk.js"
+  },
+  {
+    "revision": "16f231715f304581e1b1",
+    "url": "/lazy-image-react-tutorial/static/js/runtime-main.f3ab26b6.js"
+  }
+]);
